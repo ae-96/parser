522d3e35155adce943b8a7b85d21ab24f3b51384
Ahmed Emad <1600137@eng.asu.edu.eg>
2020-12-25 11:32:06 +0200

    add tree and node classes

---
+++ Node.py
@@ -0,0 +1,6 @@
+class node :
+    def __init__(self):
+        self.name=''
+        self.shape=''
+        self.listofseb=[]
+        self.listofchild=[]
---
+++ Tree.py
@@ -0,0 +1,4 @@
+from Node import node
+class tree :
+    def __init__(self):
+        self.root= node()
