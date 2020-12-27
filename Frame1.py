6b55c98b1e6528b4f24100c67f1dfbe36054cc65
Ahmed Emad <1600137@eng.asu.edu.eg>
2020-12-27 17:54:41 +0200

    nothing imp

--- Frame1.py
+++ Frame1.py
@@ -1,10 +1,13 @@
 from tkinter import *
-from  Frame2 import  frame2
-root=Tk()
+from Frame2 import frame2
+
+root = Tk()
 root.title("Parser")
-f=Frame(root)
+f = Frame(root)
 f.pack()
-i=frame2(f)
+i = frame2(f)
+
+
 def reset():
     global f
     f.pack_forget()
@@ -12,6 +15,8 @@ def reset():
     f = Frame(root)
     f.pack()
     i = frame2(f)
-reset_b = Button(root, text=" Reset ", command=reset,width=5 , height= 1,font="arial 12 italic")
+
+
+reset_b = Button(root, text=" Reset ", command=reset, width=5, height=1, font="arial 12 italic")
 reset_b.pack(side=BOTTOM)
 root.mainloop()
--- Frame2.py
+++ Frame2.py
@@ -1,12 +1,14 @@
 from tkinter import *
 import tkinter.messagebox
-from  Frame3 import  frame3
-from  tkinter import  filedialog
+from Frame3 import frame3
+from tkinter import filedialog
 import os.path
 from os import path
-class frame2 :
-    def __init__(self,master):
-        self.input=""
+
+
+class frame2:
+    def __init__(self, master):
+        self.input = ""
         self.newmaster = master
         self.master1 = Frame(master)
         self.master1.pack(side=TOP)
@@ -25,27 +27,30 @@ class frame2 :
         self.OK_Button.grid(row=0, column=3)
         self.browse = Button(self.master, text=" Browse File ", command=self.browse, font="arial 12 italic", width=10)
         self.browse.grid(row=1, column=3)
+
     def filedialog(self):
-        self.filename=filedialog.askopenfilename(title="Select A File",filetype=(("txt","*.txt"),("txt","*.txt")))
+        self.filename = filedialog.askopenfilename(title="Select A File", filetype=(("txt", "*.txt"), ("txt", "*.txt")))
         return self.filename
+
     def Done(self):
 
         self.input = self.text.get("0.0", "end")
         if self.input == "\n":
             tkinter.messagebox.showinfo("error", "ERROR:Empty Entry")
-        else :
+        else:
             self.master.pack_forget()
             self.master.destroy()
             self.master1.pack_forget()
             self.master1.destroy()
             self.master2.pack_forget()
             self.master2.destroy()
-            self.f = frame3(self.newmaster,self.input)
+            self.f = frame3(self.newmaster, self.input)
             self.f.show_Label(self.input)
+
     def OK(self):
         self.File_Path = self.path_entry.get()
-        condition= path.isfile(self.File_Path)
-        if (condition )==False:
+        condition = path.isfile(self.File_Path)
+        if (condition) == False:
             tkinter.messagebox.showinfo("error", "Please Enter valid path")
         else:
             print(self.File_Path)
@@ -61,11 +66,11 @@ class frame2 :
                     self.master.destroy()
                     self.master1.pack_forget()
                     self.master1.destroy()
-                    ff = open(self.File_Path, "r",)
+                    ff = open(self.File_Path, "r", )
 
                     contents = ff.read()
                     ff.close()
-                    self.f = frame3(self.newmaster,  contents)
+                    self.f = frame3(self.newmaster, contents)
                     self.f.show_Label(contents)
 
     def browse(self):
@@ -74,9 +79,9 @@ class frame2 :
         if self.File_Path == "":
             tkinter.messagebox.showinfo("error", "Please choose text file")
         else:
-            if self.File_Path [-3:] != "txt":
+            if self.File_Path[-3:] != "txt":
                 tkinter.messagebox.showinfo("error", "Please choose Proper file extension (.txt)")
-            else :
+            else:
                 self.master2.pack_forget()
                 self.master2.destroy()
                 self.master.pack_forget()
@@ -89,7 +94,6 @@ class frame2 :
                 self.f = frame3(self.newmaster, contents)
                 self.f.show_Label(contents)
 
-
     def show_Label(self):
 
         def myfunction(event):
@@ -97,16 +101,14 @@ class frame2 :
 
         canvas = Canvas(self.master1)
         self.scroll = Frame(self.master1)
-        self.scroll.pack(side=BOTTOM,fill="x")
+        self.scroll.pack(side=BOTTOM, fill="x")
         self.frame = Frame(canvas)
         myscrollbar = Scrollbar(self.master1, command=canvas.yview)
         canvas.configure(yscrollcommand=myscrollbar.set)
         canvas.create_window((0, 0), window=self.frame, anchor='nw')
         self.frame.bind("<Configure>", myfunction)
-        myscrollbar.pack(side=RIGHT,fill="y")
+        myscrollbar.pack(side=RIGHT, fill="y")
         canvas.pack(side=RIGHT)
-
-
 
         myscrollbar1 = Scrollbar(self.scroll, orient="horizontal", command=canvas.xview)
         canvas.configure(xscrollcommand=myscrollbar1.set)
@@ -115,7 +117,5 @@ class frame2 :
         myscrollbar1.pack(side="bottom", fill="x")
         canvas.pack(side="bottom")
 
-
-        self.text=Text(self.frame,width=500,height=600)
+        self.text = Text(self.frame, width=500, height=600)
         self.text.pack(side=LEFT)
-
